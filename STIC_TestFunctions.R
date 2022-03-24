97bab7ffae4525039d364406d49f16c3bb21940e
Sam Zipper <samzipper@ku.edu>
2022-03-24 12:35:07 -0500

    Test wetdry function

--- STIC_TestFunctions.R
+++ STIC_TestFunctions.R
@@ -36,13 +36,21 @@ tidy_data_with_spc <- apply_calibration(stic_data = tidy_data, calibration = cal
 
 head(tidy_data_with_spc)
 plot(tidy_data_with_spc$conductivity_uncal, tidy_data_with_spc$SpC)
+plot(tidy_data_with_spc$datetime, tidy_data_with_spc$SpC)
 
 tidy_data_with_spc_exp <- apply_calibration(stic_data = tidy_data, calibration = cal_fit_exp) # check exponential
 
 head(tidy_data_with_spc_exp)
 plot(tidy_data_with_spc_exp$conductivity_uncal, tidy_data_with_spc_exp$SpC)
 
-
 # Step 4: Classify
+source("classify_wetdry.R")  # load function
 
-classified <- classify_wetdry(stic_data_with_spc)
+tidy_data_classified <- classify_wetdry(stic_data = tidy_data_with_spc, classify_var = "nocolumn") # test error
+tidy_data_classified <- classify_wetdry(stic_data = tidy_data_with_spc, classify_var = "SpC") # test classification
+
+head(tidy_data_classified)
+
+library(ggplot2)
+ggplot(tidy_data_classified, aes(x = datetime, y = SpC, color = wetdry)) +
+  geom_point()
--- classify_wetdry.R
+++ classify_wetdry.R
@@ -1,12 +1,22 @@
 # classify_wetdry.R
-# Classify wetdry function (start with absolute threshold)
-# inputs: data frame with columns "datetime", "temperature", "conductivity", and "specific_conductivity"
+# Function to classify STIC data into a binary "wet" and "dry" column. 
+# inputs:
+#  - stic_data = data frame with STIC data, such as that produced by `apply_calibration` or `tidy_hobo_data`
+#  - classify_var = column name of the column in data frame you want to use for classification. 
+#                   Defaults to "SpC" which would be the output from `apply_calibration` function.
+#  - threshold = threshold for classifying wet vs dry
 # output: same data frame as input, but with a new column called wetdry
 
-classify_wetdry <- function(input, threshold = 1000) {
+classify_wetdry <- function(stic_data, classify_var = "SpC", threshold = 200) {
   
-input <- input %>% 
-  dplyr::mutate(wetdry = if_else(conductivity_uncal >= threshold, "wet", "dry" ))
+# extract classify variable
+if (!(classify_var %in% names(stic_data))) stop(paste0("classify_var input (", classify_var, ") is not present in stic_data"))
+class_var <- stic_data[ ,classify_var]
+
+# classify and add to data frame
+stic_data$wetdry <- if_else(class_var >= threshold, "wet", "dry" )
+
+return(stic_data)
 
 }
 

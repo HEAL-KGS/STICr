6b091c742b65e929b4d1ffa1f0b2031e2fd9c9e8
Christopher Wheeler <christopher.wheeler@ku.edu>
2022-06-30 12:14:51 -0500

    added calibration data documentation

---
+++ R/TestData_20946471_Calibration.R
@@ -0,0 +1,15 @@
+#' TestData_20946471_Calibration
+#'
+#' Calibration data for STIC sensor for conversion from uncalibrated conductivity to SpC
+#'
+#' @docType data
+#'
+#' @usage data(TestData_20946471_Calibration)
+#'
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' calibration_data <- read.csv("data/TestData_20946471_Calibration.csv")
+

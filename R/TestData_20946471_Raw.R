6eb0aac1b8225a10505e3fe586a18d8410285945
Christopher Wheeler <christopher.wheeler@ku.edu>
2022-06-30 11:40:59 -0500

    added data documentation

---
+++ R/TestData_20946471_Raw.R
@@ -0,0 +1,15 @@
+#' TestData_20946471_Raw
+#'
+#' Raw HOBO data for one STIC
+#'
+#' @docType data
+#'
+#' @usage data(TestData_20946471_Raw)
+#'
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(TestData_20946471_Raw)
+

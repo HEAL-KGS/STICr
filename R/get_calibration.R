598d08c8718464e4b266247c0a5ae7cbb61418ac
Christopher Wheeler <christopher.wheeler@ku.edu>
2022-07-01 09:33:08 -0500

    expanded get_calibration description

--- R/get_calibration.R
+++ R/get_calibration.R
@@ -1,9 +1,11 @@
 #' get_calibration
 #'
+#' @description Function to fit SpC standards and uncalibrated conductivity measured by the STIC to a model object. This model will then be used to predict SpC values using 'apply_calibration'
+#'
 #' @param calibration_data STIC calibration data frame with columns "standard" and "conductivity_uncal"
 #' @param method method for creating interpolation: "linear" (default) or "exponential"
 #'
-#' @return fitted model object
+#' @return A fitted model object relating SpC to the uncalibrated conductivity values measured by the STIC
 #' @export
 #'
 #' @examples calibration_data <- calibration_standard_data

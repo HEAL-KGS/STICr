e376135f3a2053e669cc4e0f0e9ec88fdfb23ecc
Christopher Wheeler <christopher.wheeler@ku.edu>
2022-07-02 10:35:41 -0500

    fixed typo in apply_calibration description

--- R/apply_calibration.R
+++ R/apply_calibration.R
@@ -5,7 +5,7 @@
 #' @param stic_data A data frame with a column named `conductivity_uncal`, for example as produced by the function `tidy_hobo_data`.
 #' @param calibration a model object relating `conductivity_uncal` to a standard of some sort, for example as produced by the function `get_calibration`.
 #'
-#' @return The same data frame as input, except with a new column called `SpC`. This will be in the same units as the data used to develop the model calibration.
+#' @return The same data frame as input, except with a new column called `spc`. This will be in the same units as the data used to develop the model calibration.
 #' @export
 #'
 #' @examples stic_data <- tidy_stic_data

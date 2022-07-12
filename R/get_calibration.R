6c423431661a06100ce73beb6fe6e38fde4db0a9
Christopher Wheeler <christopher.wheeler@ku.edu>
2022-07-12 15:20:36 -0500

    moved log outside of lm and got same test result

--- R/get_calibration.R
+++ R/get_calibration.R
@@ -16,7 +16,9 @@ get_calibration <- function(calibration_data, method = "linear") {
 
   if (method == "exponential") {
 
-    calibration <- lm(log(standard) ~ conductivity_uncal, data = calibration_data)
+    log_standard <- log(standard)
+
+    calibration <- lm(log_standard ~ conductivity_uncal, data = calibration_data)
 
   } else if (method == "linear") {
 

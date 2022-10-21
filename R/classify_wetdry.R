e94382383d09b078c189340f25633c7a1c7a3365
Christopher Wheeler <christopher.wheeler@ku.edu>
2022-10-21 12:20:14 -0500

    tested and updated documentation for "y-intercept" arg in classify function

--- R/classify_wetdry.R
+++ R/classify_wetdry.R
@@ -5,7 +5,7 @@
 #' @param stic_data A data frame with STIC data, such as that produced by `apply_calibration` or `tidy_hobo_data`
 #' @param classify_var Name of the column in data frame you want to use for classification.
 #                   Defaults to `SpC` which would be the output from `apply_calibration` function.
-#' @param method User chooses which classification method used to generate the binary data. "absolute" uses an absolute numerical threshold for classifying wet vs dry. "percent" uses a threshold based on a given percentage of the maximum value of the classification variable in the data frame.
+#' @param method User chooses which classification method used to generate the binary data. "absolute" uses an absolute numerical threshold for classifying wet vs dry. "percent" uses a threshold based on a given percentage of the maximum value of the classification variable in the data frame. "y-intercept" uses the y-intercept from the `get_calibration` function
 #' @param threshold This is the user-defined threshold for determining wet versus dry based on the designated classification variable. If using the "absolute" method, the threshold will be a value in the same units as the designated classification variable. If using the "percent" method, the value will be a decimal percentage of the max value of the classification variable in the data frame. Values above this proportion of the maximum will be designated as wet.
 #'
 #' @return The same data frame as input, but with a new column called `wetdry`.

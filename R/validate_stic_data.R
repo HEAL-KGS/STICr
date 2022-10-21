aeb8f693c076e082e600d4589d6caf58797d88c2
Christopher Wheeler <christopher.wheeler@ku.edu>
2022-10-21 14:44:48 -0500

    validation function draft

---
+++ R/validate_stic_data.R
@@ -0,0 +1,61 @@
+#' validate_stic_data.R
+#'
+#' @description This function creates two confusion matrices from a classified STIC data frame with the variable names of that produced by `classify_wetdry`, with the addition of two additional columns called `field_SpC` and `field_classification`
+#'
+#' @param stic_data classified STIC data frame with the variable names of that produced by `classify_wetdry`, with the addition of two additional columns called `field_SpC` and `field_classification`
+#' @param include_SpC logical argument where the user specifies if they want a matrix for SpC measurements
+#' @param include_classification logical argument where the user specifies if they want a matrix for SpC measurements
+#'
+#' @return one or two confusion matrices
+#' @export
+#'
+#' @examples trimmed_data <- trim_hobo_data(tidy_stic_data, time_start = "2021-07-16 18:00:00", time_end = "2021-07-27 01:00:00")
+#' head(trimmed_data)
+
+validate_stic_data <- function(stic_data, include_SpC = TRUE, include_classification = TRUE) {
+
+  require(caret)
+
+  if (include_SpC == TRUE) {
+
+    expected_value_SpC <- stic_data$field_SpC
+    predicted_value_SpC <- stic_data$SpC
+
+    SpC_confusion_matrix <- confusionMatrix(data = predicted_value_SpC,
+                                            reference = expected_value_SpC)
+
+  }
+
+  if (include_classification == TRUE) {
+
+    expected_value_classification <- stic_data$field_classification
+    predicted_value_classification <- stic_data$wetdry
+
+    classification_confusion_matrix <- confusionMatrix(data = predicted_value_classification,
+                                            reference = expected_value_classification)
+
+  }
+
+  if (include_SpC == TRUE & include_classification == TRUE) {
+
+    confusion_matrix <- list(SpC_confusion_matrix, classification_confusion_matrix)
+
+  } else if (include_classification == TRUE & include_SpC != TRUE) {
+
+    confusion_matrix <- classification_confusion_matrix
+
+} else {
+
+
+  confusion_matrix <- SpC_confusion_matrix
+
+
+}
+
+return(confusion_matrix)
+
+
+}
+
+
+

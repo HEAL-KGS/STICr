11bad11e31ea9240ad7d49ffbc60d138460b5dd0
Christopher Wheeler <christopher.wheeler@ku.edu>
2022-10-21 12:48:24 -0500

    adding field obs dataset for validation function example

---
+++ R/classified_stic_data_with_field_obs.R
@@ -0,0 +1,14 @@
+#' classified_stic_data_with_field_obs
+#'
+#' Classified STIC data with two additional field validation columns
+#'
+#' @docType data
+#'
+#' @usage data(classified_stic_data_with_field_obs)
+#'
+#'
+#' @keywords datasets
+#'
+#'
+
+

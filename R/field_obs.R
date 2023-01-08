87c51d9866be794f3e06bdb601d6fa2941a3d656
Christopher Wheeler <christopher.wheeler@ku.edu>
2023-01-08 11:37:20 -0600

    adding example field verification data

---
+++ R/field_obs.R
@@ -0,0 +1,14 @@
+#' field_obs
+#'
+#' Made up field verification data; must include columns called `datetime` (in Posixct format), `wetdry` as a character, and optionally, a numeric column called `SpC`
+#'
+#' @docType data
+#'
+#' @usage data(field_obs)
+#'
+#'
+#' @keywords datasets
+#'
+#'
+
+

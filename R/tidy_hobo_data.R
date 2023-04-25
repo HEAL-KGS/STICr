7bf067b5a5cbd927add6f6e57b7688ec2c506919
Christopher Wheeler <christopher.wheeler@ku.edu>
2023-04-25 11:18:51 -0500

    added in code to deal with imperial values

--- R/tidy_hobo_data.R
+++ R/tidy_hobo_data.R
@@ -18,6 +18,8 @@
 #' head(clean_data)
 #'
 
+infile <- "20210915_20946486_STIC.csv"
+
 tidy_hobo_data <- function(infile, outfile = FALSE, convert_utc = TRUE) {
 
   # read in file
@@ -26,10 +28,51 @@ tidy_hobo_data <- function(infile, outfile = FALSE, convert_utc = TRUE) {
   # get numeric version of number of hours to add from datetime column name
   utc_time_offset <-
     raw_data |>
-    dplyr::select(contains("Date")) |>
+    dplyr::select(contains("Time")) |>
     colnames() |>
     str_sub(start = -5, end = -4) |>
     as.numeric()
+
+  if ("Date" %in% names(raw_data)) {
+    logger_record$datetime <- mdy_hms(paste0(logger_record$Date, " ", logger_record$Time..GMT.05.00))
+  } else {
+    logger_record$datetime <- mdy_hms(logger_record$Date.Time..GMT.05.00)
+  }
+
+  if ("lum" %in% names(raw_data)) {
+
+    tidy_data <-
+      raw_data |>
+      dplyr::rename_with(.cols = contains("Temp"),
+                         .fn = function(x){"tempC"}) |>
+      dplyr::rename_with(.cols = contains("Intensity"),
+                         .fn = function(x){"condUncal"}) |>
+      dplyr::rename_with(.cols = contains("Date"),
+                         .fn = function(x){"datetime"}) |>
+      dplyr::select(datetime, condUncal, tempC) |>
+      dplyr::mutate(datetime = lubridate::mdy_hms(datetime)) |>
+      dplyr::mutate(tempC = as.numeric(tempC),
+                    condUncal = gsub(",", "", condUncal),
+                    condUncal = as.numeric(condUncal)) |>
+      dplyr::mutate(tempC = (tempC - 32) * 5/9) |>
+      dplyr::mutate(condUncal = 10.7639104167 * condUncal)
+
+  } else {
+
+    tidy_data <-
+      raw_data |>
+      dplyr::rename_with(.cols = contains("Temp"),
+                         .fn = function(x){"tempC"}) |>
+      dplyr::rename_with(.cols = contains("Intensity"),
+                         .fn = function(x){"condUncal"}) |>
+      dplyr::rename_with(.cols = contains("Date"),
+                         .fn = function(x){"datetime"}) |>
+      dplyr::select(datetime, condUncal, tempC) |>
+      dplyr::mutate(datetime = lubridate::mdy_hms(datetime)) |>
+      dplyr::mutate(tempC = as.numeric(tempC),
+                    condUncal = gsub(",", "", condUncal),
+                    condUncal = as.numeric(condUncal))
+  }
 
   # tidy columns and names
   tidy_data <-
